29616e9996d1529193fe540cff776e4558e5ed98
Valeriu1 <74401589+Valeriu1@users.noreply.github.com>
2023-05-18 13:57:44 +0200

    adding actor page and testing App with API

--- SEP6/Program.cs
+++ SEP6/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using SEP6;
+using SEP6.Data.Actors;
 using MudBlazor.Services;
 
 
@@ -10,5 +11,6 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddMudServices();
+builder.Services.AddSingleton<IActorService, ActorService>();
 
 await builder.Build().RunAsync();
