8ddfb4d34f3ed3ed2ec80f3c030057235c952c2c
StapanulLumii <82084902+StapanulLumii@users.noreply.github.com>
2023-05-18 11:56:01 +0200

    Added Movie Service interface and implementation of it

---
+++ SEP6/Data/Movies/MovieService.cs
@@ -0,0 +1,52 @@
+﻿using SEP6.Models;
+using System.Text.Json;
+
+namespace SEP6.Data.Movies
+{
+    public class MovieService : IMovieService
+    {
+
+        string url = "https://localhost:7154/movie";
+
+        HttpClient client;
+        private int movieId = 0;
+
+        public MovieService()
+        {
+            client = new HttpClient();
+        }
+
+        public async Task<Movie> GetMovieByID(int id)
+        {
+            string message = await client.GetStringAsync(url + "/" + id);
+            Movie result = JsonSerializer.Deserialize<Movie>(message);
+            return result;
+        }
+
+        public int GetMovieId()
+        {
+            return movieId;
+        }
+
+        public void SetMovieId(int id)
+        {
+            movieId = id;
+        }
+        public async Task<ListOfMovies> GetMoviesBySearch(int page, string query)
+        {
+            string message = await client.GetStringAsync(url + "/search?page=" + page + "&query=" + query);
+            ListOfMovies results = JsonSerializer.Deserialize<ListOfMovies>(message);
+            return results;
+        }
+
+        public Task<ListOfMovies> GetMoviesByRating(int page)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ListOfMovies> GetMoviesByTitle(int page)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ SEP6/Models/ListOfMovies.cs
@@ -0,0 +1,11 @@
+﻿namespace SEP6.Models
+{
+    public class ListOfMovies
+    {
+        public int CurrentPage { get; set; }
+
+        public List<Movie> movies { get; set; }
+
+        public int TotalPage { get; set; }
+    }
+}
