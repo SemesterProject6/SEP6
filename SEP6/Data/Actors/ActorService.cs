3c05105df34a35dcae6dc5d22370c55f159f3641
Valeriu1 <74401589+Valeriu1@users.noreply.github.com>
2023-05-18 11:56:40 +0200

    adding get actor service

---
+++ SEP6/Data/Actors/ActorService.cs
@@ -0,0 +1,34 @@
+﻿using SEP6.Models;
+using System.Text.Json;
+
+namespace SEP6.Data.Actors
+{
+    public class ActorService : IActorService
+    {
+        string url = "https://localhost:7154/actor";
+        HttpClient client;
+        private int actorId = 0;
+
+        public ActorService()
+        {
+            client = new HttpClient();
+        }
+
+        public async Task<Actor> GetActorByID(int id)
+        {
+            string message = await client.GetStringAsync(url + "/" + id);
+            Actor result = JsonSerializer.Deserialize<Actor>(message);
+            return result;
+        }
+
+        public int GetActorId()
+        {
+            return actorId;
+        }
+
+        public void SetActorId(int id)
+        {
+            actorId = id;
+        }
+    }
+}
