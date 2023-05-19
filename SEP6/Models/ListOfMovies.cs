c1af4b8e456b4acaea33280cb792e4d2530fad0f
StapanulLumii <82084902+StapanulLumii@users.noreply.github.com>
2023-05-19 13:39:14 +0200

    Search a movie by its title

--- SEP6/Models/ListOfMovies.cs
+++ SEP6/Models/ListOfMovies.cs
@@ -1,11 +1,14 @@
-﻿namespace SEP6.Models
+﻿using System.Text.Json.Serialization;
+
+namespace SEP6.Models
 {
     public class ListOfMovies
     {
+        [JsonPropertyName("page")]
         public int CurrentPage { get; set; }
-
-        public List<Movie> movies { get; set; }
-
+        [JsonPropertyName("results")]
+        public List<Movie> Movies { get; set; }
+        [JsonPropertyName("total_pages")]
         public int TotalPage { get; set; }
     }
 }
