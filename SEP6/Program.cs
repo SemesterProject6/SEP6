969df15d87e9bc9dcf5fb57b7c8d55454b835fd8
Valeriu1 <74401589+Valeriu1@users.noreply.github.com>
2023-05-26 11:48:26 +0200

    Creating Login and Register

--- SEP6/Program.cs
+++ SEP6/Program.cs
@@ -4,7 +4,9 @@ using SEP6;
 using SEP6.Data.Actors;
 using SEP6.Data.Movies;
 using MudBlazor.Services;
-
+using Microsoft.AspNetCore.Components.Authorization;
+using SEP6.Models;
+using SEP6.Data.Users;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -12,7 +14,11 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddMudServices();
+builder.Services.AddOptions();
+builder.Services.AddAuthorizationCore();
 builder.Services.AddSingleton<IActorService, ActorService>();
 builder.Services.AddSingleton<IMovieService, MovieService>();
+builder.Services.AddSingleton<IUserService, UserService>();
+builder.Services.AddScoped<AuthenticationStateProvider, CustomAuthenticationStateProvider>();
 
 await builder.Build().RunAsync();
